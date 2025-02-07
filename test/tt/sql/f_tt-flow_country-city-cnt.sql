080b543a0300819f4107a868e84b17e4aa0fc345
gbm <gbm@gbm.com>
2025-02-07 11:52:19 +0000

    fixed group by statement

--- test/tt/sql/f_tt-flow_country-city-cnt.sql
+++ test/tt/sql/f_tt-flow_country-city-cnt.sql
@@ -1,4 +1,5 @@
 drop table if exists iceberg.gbm.sm_country_city purge;
 
 create table iceberg.gbm.sm_country_city as
-select country_id,count(1) cnt from iceberg.gbm.extr_city;
+select country_id,count(1) cnt from iceberg.gbm.extr_city
+group by country_id;
